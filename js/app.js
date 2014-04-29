87d3921434dce24db51afbea07d8ac6819891f95
dorahagen <dhagen@dooleyandassociates.com>
2014-04-29 13:04:23 -0500

    2nd Check In

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-ready').show();
 	});
 	$('.ryu').mouseleave(function() {
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-still').show();
 	});
 	$('.ryu').mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show();
-		$('.hadouken').animate (
+		$('.hadouken').finish().show()
+		.animate (
 			{'left': '300px'},
 			500,
 			function() {
@@ -23,9 +26,26 @@ $(document).ready(function() {
 	});
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-ready').show();
 	});
-})
+	$('body').keydown(function(event){
+		if(event.keyCode == 88){
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ruy-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	});
+	$('body').keyup(function(event){
+		if(event.keyCode == 88){
+			$('.ryu-cool').hide();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-ready').show();
+		}
+	});
+});
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
